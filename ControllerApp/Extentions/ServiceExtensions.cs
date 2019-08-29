ce416b3eff1ef9b7314b72ac77aa7446703faca2
Stanley Mohlala <Stanley@moiponefleet.co.za>
2019-08-29 11:03:30 +0200

    Logic to setup core services

---
+++ ControllerApp/Extentions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection; 
+
+namespace ControllerApp.Extentions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
